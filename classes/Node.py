f2c0f55e9a121e7aecfe1c03d0f7f44a99821139
Federico Micelli <federico.micelli@studenti.polito.it>
2020-11-18 18:58:28 +0100

    create Node class
    Co-Authored-By: simomaiden <62210127+simomaiden@users.noreply.github.com>
    Co-Authored-By: FrSp13 <62210287+FrSp13@users.noreply.github.com>

---
+++ classes/Node.py
@@ -0,0 +1,46 @@
+from Lightpath import *
+
+class Node:
+    def __init__(self, initial_data):
+        self._label = initial_data['label']
+        self._position = initial_data['position']
+        self._connected_nodes = initial_data['connected_nodes']
+        self._successive = {}   # dict of Line objects
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def position(self):
+        return self._position
+
+    @property
+    def connected_nodes(self):
+        return self._connected_nodes
+
+    @property
+    def successive(self):
+        return self._successive
+
+    @label.setter
+    def label(self, value):
+        self._label = value
+
+    @position.setter
+    def position(self, value):
+        self._position = value
+
+    @connected_nodes.setter
+    def connected_nodes(self, value):
+        self._connected_nodes = value
+
+    @successive.setter
+    def successive(self, value):
+        self._successive = value
+
+    def propagate(self, propagated_object):
+        propagated_object.update_path()
+        if len(propagated_object.path) != 0:
+            next_line_label = self.label + propagated_object.path[0]
+            self.successive[next_line_label].propagate(propagated_object)
