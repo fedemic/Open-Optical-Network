4699b91502c5424f5800967beb040a7df1804618
simomaiden <spocksimo@gmail.com>
2021-01-11 17:21:21 +0100

    Update task and results (snr latency 100 random connections)
    Co-Authored-By: FrSp13 <62210287+FrSp13@users.noreply.github.com>
    Co-Authored-By: Federico Micelli <48220610+federico-micelli@users.noreply.github.com>

--- tasks/distributions_100_random_connections.py
+++ tasks/distributions_100_random_connections.py
@@ -1,62 +1,72 @@
 import sys
 sys.path.insert(1, '../core')
 from Network import *
-import random
-import matplotlib.pyplot as plt
 
-# network generation
-net = Network("../resources/nodes_not_full.json")
-net.connect()
-net.draw()
-net.create_weighted_paths()
-net.create_route_space()
+for i in range(2):
+    if i == 0:
+        file_name = "nodes_not_full"
+    else:
+        file_name = "nodes_full"
 
-# 100 random requests creation
-node_list = list(net.nodes.keys())
-conn_list = []
-initial_data = {}
-signal_power = 1
+    # network generation
+    net = Network("../resources/"+file_name+".json")
+    net.connect()
+    net.draw()
+    net.create_weighted_paths()
+    net.create_route_space()
 
-for i in range(N_CONNECTIONS):
-    inout_nodes = random.sample(node_list, 2)
+    # random connections generation on the first iteration (so the same connections are employed in the analysis)
+    if i == 0:
+        # 100 random requests creation
+        node_list = list(net.nodes.keys())
+        conn_list = []
+        initial_data = {}
+        signal_power = 1
 
-    initial_data["input"] = inout_nodes[0]
-    initial_data["output"] = inout_nodes[1]
-    initial_data["signal_power"] = signal_power
+        for j in range(N_CONNECTIONS):
+            inout_nodes = random.sample(node_list, 2)
 
-    conn_list.append(Connection(initial_data))
+            initial_data["input"] = inout_nodes[0]
+            initial_data["output"] = inout_nodes[1]
+            initial_data["signal_power"] = signal_power
 
-# request deployment optimizing latency
-net.stream(conn_list, signal_power, optimize="latency")
+            conn_list.append(Connection(initial_data))
 
-latency_values = []
-for connection in conn_list:
-    latency_values.append(connection.latency)
+    # request deployment optimizing latency
+    deployed_conn_list = list(conn_list)
+    net.stream(deployed_conn_list, signal_power, optimize="latency")
 
-# request deployment optimizing SNR
-net.stream(conn_list, signal_power, optimize="snr")
+    latency_values = []
+    for connection in deployed_conn_list:
+        latency_values.append(connection.latency)
 
-snr_values = []
-for connection in conn_list:
-    if connection.snr != None:
-        snr_values.append(connection.snr)
+    # reset of the lines states and switching matrices
+    net.reset_network()
 
-# results distribution plot
-plt.figure()
-plt.hist(latency_values)
-plt.xlabel("Latency [s]")
-plt.ylabel("Occurences")
-plt.title("Latency Distribution")
-plt.savefig('../results/latency_distribution_nodes_full.png')
+    # request deployment optimizing SNR
+    deployed_conn_list = list(conn_list)
+    net.stream(deployed_conn_list, signal_power, optimize="snr")
 
-plt.figure()
-plt.hist(snr_values, color="r")
-plt.xlabel("SNR [dB]")
-plt.ylabel("Occurences")
-plt.title("SNR Distribution")
-plt.savefig('../results/snr_distribution_nodes_not_full.png')
+    snr_values = []
+    for connection in deployed_conn_list:
+        if connection.snr != None:
+            snr_values.append(connection.snr)
 
+    # results distribution plot
+    plt.figure()
+    plt.hist(latency_values)
+    plt.xlabel("Latency [s]")
+    plt.ylabel("Occurences")
+    plt.title("Latency Distribution")
+    plt.savefig("../results/snr_latency_100_random/"+file_name+"/latency_distribution.png")
 
-# NON RICHIESTA
-net.route_space.to_csv('../results/route_space.csv')
-net.weighted_paths.to_csv('../results/weigthed_paths.csv')
+    plt.figure()
+    plt.hist(snr_values, color="r")
+    plt.xlabel("SNR [dB]")
+    plt.ylabel("Occurences")
+    plt.title("SNR Distribution")
+    plt.savefig("../results/snr_latency_100_random/"+file_name+"/snr_distribution.png")
+
+    # Not requested
+    net.route_space.to_csv("../results/snr_latency_100_random/"+file_name+"/route_space.csv")
+    net.weighted_paths.to_csv("../results/snr_latency_100_random/"+file_name+"/weighted_paths.csv")
