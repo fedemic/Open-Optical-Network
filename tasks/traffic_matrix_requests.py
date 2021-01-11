f6e02574282f1bda888e67dda0da2625b7946e7d
simomaiden <spocksimo@gmail.com>
2021-01-11 17:22:48 +0100

    Update task and results (traffic matrix)
    Co-Authored-By: FrSp13 <62210287+FrSp13@users.noreply.github.com>
    Co-Authored-By: Federico Micelli <48220610+federico-micelli@users.noreply.github.com>

--- tasks/traffic_matrix_requests.py
+++ tasks/traffic_matrix_requests.py
@@ -14,7 +14,7 @@ for i in range(3):
     net.connect()
     net.create_weighted_paths()
     net.create_route_space()
-    M = 1
+    M = 50
 
     n_nodes = len(net.nodes)
     traffic_matrix = np.zeros((n_nodes, n_nodes))
@@ -26,13 +26,21 @@ for i in range(3):
         net.reset_network()
         M += 1
 
+    with open("../results/traffic_matrix/"+transceiver+"/results.txt", 'w') as file:
+        file.write(transceiver.capitalize() + " DATA\n")
+        file.write("Maximum M: " + str(M-2)+"\n")
+        file.write("Maximum bit rate: " + str(get_max_bitrate(conn_list)/1e9) + " Gpbs\n")
+        file.write("Minimum bit rate: " + str(get_min_bitrate(conn_list)/1e9) + " Gbps\n")
+        file.write("Average bit rate: " + str(get_average_bitrate(conn_list)/1e9) + " Gbps")
+
     print(transceiver.capitalize() + " DATA")
     print("Maximum M: " + str(M-2))
     print("Maximum bit rate: " + str(get_max_bitrate(conn_list)/1e9) + " Gpbs")
     print("Minimum bit rate: " + str(get_min_bitrate(conn_list)/1e9) + " Gbps")
     print("Average bit rate: " + str(get_average_bitrate(conn_list)/1e9) + " Gbps")
-    plot_distribution(conn_list, "bit_rate", "../results/"+transceiver+"/bit_rate_distribution.png")
-    plot_distribution(conn_list, "latency", "../results/"+transceiver+"/latency_distribution.png")
 
-    net.route_space.to_csv("../results/"+transceiver+"/route_space.csv")
-    net.weighted_paths.to_csv("../results/"+transceiver+"/weigthed_paths.csv")
+    plot_distribution(conn_list, "bit_rate", "../results/traffic_matrix/"+transceiver+"/bit_rate_distribution.png")
+    plot_distribution(conn_list, "latency", "../results/traffic_matrix/"+transceiver+"/latency_distribution.png")
+
+    net.route_space.to_csv("../results/traffic_matrix/"+transceiver+"/route_space.csv")
+    net.weighted_paths.to_csv("../results/traffic_matrix/"+transceiver+"/weigthed_paths.csv")
